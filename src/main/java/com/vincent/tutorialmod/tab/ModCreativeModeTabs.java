b3518c58d2251419429152438a7b462c8161f63b
vincent-0-tnecniv <ngvincent2020@gmail.com>
2026-07-31 21:12:09 +0800

    Lesson #4

--- src/main/java/com/vincent/tutorialmod/tab/ModCreativeModeTabs.java
+++ src/main/java/com/vincent/tutorialmod/tab/ModCreativeModeTabs.java
@@ -1,14 +1,41 @@
 package com.vincent.tutorialmod.tab;
 
 import com.vincent.tutorialmod.TutorialMod;
+import com.vincent.tutorialmod.item.ModItems;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.function.Supplier;
 
 public class ModCreativeModeTabs {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MOD_ID);
+
+    public static final Supplier<CreativeModeTab> AZURITE_ITEMS_TAB = CREATIVE_MODE_TABS.register("azurite_item_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.AZURITE.get()))
+                    .title(Component.translatable("creativetab.tutorialmod.azurite_items"))
+                    .withTabsBefore(CreativeModeTabs.INGREDIENTS)
+                    .withTabsAfter(Identifier.fromNamespaceAndPath(TutorialMod.MOD_ID, "azurite_block_tab"))
+                    .displayItems((_, output) -> {
+                        output.accept(ModItems.AZURITE);
+                        output.accept(ModItems.RAW_AZURITE);
+                    })
+                    .build());
+
+    public static final Supplier<CreativeModeTab> AZURITE_BLOCKS_TAB = CREATIVE_MODE_TABS.register("azurite_block_tab",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.RAW_AZURITE.get()))
+                    .title(Component.translatable("creativetab.tutorialmod.azurite_blocks"))
+                    .displayItems((_, output) -> {
+                        output.accept(ModItems.AZURITE);
+                        output.accept(ModItems.RAW_AZURITE);
+                    })
+                    .build());
 
     public static void register(IEventBus eventBus) {
         CREATIVE_MODE_TABS.register(eventBus);
