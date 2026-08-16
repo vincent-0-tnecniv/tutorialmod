386abd9b1140d2af97e01a1c6a3d6656a9850de1
vincent-0-tnecniv <ngvincent2020@gmail.com>
2026-08-16 21:35:54 +0800

    Lesson #46 - additional method to support entity tags - As block tags and structure tags require a custom condition, and is not a good convention to apply by tag, they will not be supported for now

--- src/main/java/com/vincent/tutorialmod/util/datagen/BaseGlobalLootModifierProvider.java
+++ src/main/java/com/vincent/tutorialmod/util/datagen/BaseGlobalLootModifierProvider.java
@@ -1,27 +1,38 @@
 package com.vincent.tutorialmod.util.datagen;
 
+import com.vincent.tutorialmod.datagen.loot.ModExtraLootProvider;
+import com.vincent.tutorialmod.tags.ModTags;
+import net.minecraft.advancements.predicates.BlockPredicate;
+import net.minecraft.advancements.predicates.StatePropertiesPredicate;
+import net.minecraft.advancements.predicates.entity.EntityPredicate;
+import net.minecraft.advancements.predicates.entity.EntityTypePredicate;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.predicates.AnyOfCondition;
-import net.minecraft.world.level.storage.loot.predicates.LootItemBlockStatePropertyCondition;
-import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
+import net.minecraft.world.level.storage.loot.predicates.*;
 import net.neoforged.neoforge.common.data.GlobalLootModifierProvider;
 import net.neoforged.neoforge.common.loot.AddTableLootModifier;
 import net.neoforged.neoforge.common.loot.LootTableIdCondition;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public abstract class BaseGlobalLootModifierProvider extends GlobalLootModifierProvider {
-
-
     public BaseGlobalLootModifierProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries, String modid) {
         super(output, registries, modid);
     }
@@ -31,6 +42,18 @@ public abstract class BaseGlobalLootModifierProvider extends GlobalLootModifierP
     }
 
     // TODO: add archaeology, brush, carve, charged_creeper, dispensers, equipment, gameplay, harvest, pots, shearing, spawners
+
+    protected AddTableLootModifier entityTagLoot(HolderGetter<EntityType<?>> lookup, TagKey<EntityType<?>> tag, ResourceKey<LootTable> lootTable) {
+        return customLoot(new LootItemCondition[] {
+                LootItemEntityPropertyCondition.hasProperties(
+                        LootContext.EntityTarget.THIS,
+                        EntityPredicate.Builder.entity()
+                                .entityType(EntityTypePredicate.of(lookup, tag))
+                ).build()
+        },  lootTable);
+    }
+    // For more than one tag - use another tag and add that one single tag to the loot modifier
+    // You can even remove custom mobs if doing so
 
     protected AddTableLootModifier simpleEntityLoot(List<EntityType<?>> entities, ResourceKey<LootTable> modifiedLootTable) {
         return new AddTableLootModifier(createEntityDropConditions(entities), 1000, modifiedLootTable);
@@ -69,6 +92,28 @@ public abstract class BaseGlobalLootModifierProvider extends GlobalLootModifierP
 
     private LootItemCondition[] createBlockConditions(List<Block> blocks) {
         return new LootItemCondition[] {AnyOfCondition.anyOf(blocks.stream().map(LootItemBlockStatePropertyCondition::hasBlockStateProperties).toArray(LootItemBlockStatePropertyCondition.Builder[]::new)).build()};
+    }
+
+    public static List<EntityType<?>> getEntityTypesFromTag(HolderLookup.Provider provider, TagKey<EntityType<?>> tagKey) {
+        var lookup = provider.lookupOrThrow(Registries.ENTITY_TYPE);
+        var holders = lookup.getOrThrow(tagKey);
+        return holders.stream()
+                .map(Holder::value)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Block> getBlocksFromTag(TagKey<Block> tagKey) {
+        HolderSet<Block> holders = BuiltInRegistries.BLOCK.getOrThrow(tagKey);
+        return holders.stream()
+                .map(Holder::value)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Item> getItemsFromTag(TagKey<Item> tagKey) {
+        HolderSet<Item> holders = BuiltInRegistries.ITEM.getOrThrow(tagKey);
+        return holders.stream()
+                .map(Holder::value)
+                .collect(Collectors.toList());
     }
 
     protected abstract void start();
